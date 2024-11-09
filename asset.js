5733b19162e25a98926e23cf92544792de9effba
Kosi Mensah <kmensah@usf.edu>
2024-11-09 14:40:38 -0500

    Setup Import link

--- asset.js
+++
@@ -1,9 +0,0 @@
-export const assets = [
-    { id: 1, name: 'NIO', type: 'stock', price: 120, quantity: 7 },
-    { id: 2, name: 'TSLA', type: 'stock', price: 1500, quantity: 10 },
-    { id: 3, name: 'US Corporate Bond', type: 'bond', price: 1200, quantity: 15 }
-];
-
-export function getAssetById(id) {
-    return assets.find(asset => asset.id === id);
-}
