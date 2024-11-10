1e8d4d56deb1c7dd921af2f38afd1644234318e2
Kosi Mensah <kmensah@usf.edu>
2024-11-10 13:51:17 -0500

    Create transaction module with Transaction class

---
+++ transaction.js
@@ -0,0 +1,28 @@
+// Imports assets and getAssetByID from the asset JavaScript file
+import { assets, getAssetById } from './asset.js';
+
+// Create Transaction class
+class Transaction {
+    constructor(id, type, quantity) {
+        this.id = id;
+        this.type = type;
+        this.quantity = quantity;
+        this.assets = getAssetById(id);
+    }
+
+    // Creates the function  complete a transaction
+    completeTransaction() {
+        if (this.type === 'sell') {
+            if (this.quantity > this.assets.quantity) {
+                throw new Error (`Insufficient quantity for sale of ${this.assets.name}.`);
+            }
+            this.assets.quantity -= this.quantity;
+        } else if (this.type === 'buy') {
+            this.assets.quantity += this.quantity;
+        } else {
+            throw new Error ('Invalid transaction type.');
+        }
+    }
+}
+
+export default Transaction;
